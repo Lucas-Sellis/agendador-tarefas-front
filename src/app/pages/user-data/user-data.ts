5a828f21ca1cef80d21daad3601e5dffe63c2d4e
Lucas <lucas.sellis@gmail.com>
2026-04-05 10:47:45 -0300

    Created dinamic form with modal

--- src/app/pages/user-data/user-data.ts
+++ src/app/pages/user-data/user-data.ts
@@ -1,20 +1,29 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { UserResponse, UserService } from '../../services/user';
+import { DialogField, ModalDialog } from '../../shared/components/modal-dialog/modal-dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-data',
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './user-data.html',
   styleUrl: './user-data.scss',
 })
 export class UserData {
   private formBuilder = inject(FormBuilder);
   private userService = inject(UserService);
+  readonly dialog = inject(MatDialog);
 
   // 1. Pegamos os dados do usuário
   user = this.userService.getUser();
@@ -25,10 +34,39 @@ export class UserData {
     email: [{ value: this.user?.email || '', disabled: true }],
   });
 
-  cadastrarEndereco(){
+  // cara isso aqui é uma tal de Modal foi pego la no angular material
+
+  cadastrarEndereco() {
+    const formConfig: DialogField[] = [
+      { name: 'cep', label: 'CEP', validators: [Validators.required] },
+      { name: 'rua', label: 'Rua' },
+      { name: 'numero', label: 'Número' },
+      { name: 'complement', label: 'Complemento' },
+      { name: 'cidade', label: 'Cidade' },
+      { name: 'estado', label: 'Estado' },
+    ];
+
+    const dialogRef = this.dialog.open(ModalDialog, {
+      data: { title: 'Adicionar Endereço', formConfig },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 
-  cadastrarTelefone(){
-  }
+  cadastrarTelefone() {
+    const formConfig: DialogField[] = [
+      { name: 'ddd', label: 'DDD', validators: [Validators.required] },
+      { name: 'numero', label: 'Número', validators: [Validators.required] },
+    ];
 
+    const dialogRef = this.dialog.open(ModalDialog, {
+      data: { title: 'Adicionar Telefone', formConfig },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
